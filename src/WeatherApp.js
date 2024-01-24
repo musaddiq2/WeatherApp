34efa7b556a4c1e0425cc5a58c62b162198f0117
musaddiq2 <uzumakibaruto093@gmail.com>
2024-01-24 22:53:09 +0530

    4th commit

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import "./WeatherApp.css";
 function WeatherApp() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -11,26 +11,36 @@ function WeatherApp() {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.get('https://api.weatherapi.com/v1/current.json', {
-        params: {
-          key: '236233e528fc4f228e2164243242401',
-          q: city
+      const response = await axios.get(
+        "https://api.weatherapi.com/v1/current.json",
+        {
+          params: {
+            key: "236233e528fc4f228e2164243242401",
+            q: city,
+          },
         }
-      });
-      console.log(response.data); 
+      );
+      console.log(response.data); // Log the entire response
       setWeatherData(response.data);
     } catch (error) {
-      alert('Failed to fetch weather data');
+        alert("Failed to fetch weather data");
+      console.error('Failed to fetch weather data:', error);
+      setError('Failed to fetch weather data');
     }
     setLoading(false);
   };
 
   return (
     <div>
-       <div className='weather-card'> 
-      <h1>Weather App</h1>
-      <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder='Enter City and Search'/>
-      <button onClick={fetchData}>Search</button>
+      <div className="weather-card">
+        <h1>Weather App</h1>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter City and Search"
+        />
+        <button onClick={fetchData}>Search</button>
       </div>
       {loading && <p>Loading data...</p>}
       {error && <p>{error}</p>}
